439af8bc07d2a465c4b67efc357e86c8fe1933a5
Josh Willhite <jwillhite@aerospike.com>
2022-05-12 17:00:59 -0700

    [AMS-12849] Add a test fixture for common code

--- tests/unit/plugins/modules/test_acl_common.py
+++ tests/unit/plugins/modules/test_acl_common.py
@@ -7,6 +7,9 @@ from ansible_collections.aerospike.acl.plugins.module_utils.acl_common import (
     ACLWarning,
 )
 
+@pytest.fixture
+def generic_acl():
+    return ACL("cfg_path", "user", "password", "instance")
 
 class RunMock:
     def __init__(self, cmd, returncode, stdout, stderr):
@@ -16,7 +19,7 @@ class RunMock:
         self.stderr = stderr.encode()
 
 
-def test_execute_cmd_happy(mocker):
+def test_execute_cmd_happy(mocker, generic_acl):
     commands = []
 
     def mock_run(cmd, stdout="foo", stderr="bar"):
@@ -28,9 +31,7 @@ def test_execute_cmd_happy(mocker):
         mock_run,
     )
 
-    acl = ACL("cfg_path", "user", "password", "instance")
-
-    acl.execute_cmd("this is a command")
+    generic_acl.execute_cmd("this is a command")
     assert commands[0] == [
         "asadm",
         "--config-file=cfg_path",
@@ -43,7 +44,7 @@ def test_execute_cmd_happy(mocker):
     ]
 
 
-def test_execute_cmd_error(mocker):
+def test_execute_cmd_error(mocker, generic_acl):
     commands = []
 
     def mock_run(cmd, stdout="foo", stderr="bar"):
@@ -55,10 +56,8 @@ def test_execute_cmd_error(mocker):
         mock_run,
     )
 
-    acl = ACL("cfg_path", "user", "password", "instance")
-
     with pytest.raises(ACLError) as err:
-        acl.execute_cmd("this is a command")
+        generic_acl.execute_cmd("this is a command")
 
     assert "ohh no there was an error!!" in str(err.value)
     assert commands[0] == [
@@ -73,36 +72,32 @@ def test_execute_cmd_error(mocker):
     ]
 
 
-def test_parse_error(mocker):
-    acl = ACL("cfg_path", "user", "password", "instance")
-    result = acl._parse_error(
+def test_parse_error(mocker, generic_acl):
+    result = generic_acl._parse_error(
         "\n".join(["ERROR: ohh no there was an error!!", "here's more information"])
     )
 
     assert "ERROR: ohh no there was an error!!" == result
 
 
-def test_parse_results_no_json(mocker):
-    acl = ACL("cfg_path", "user", "password", "instance")
-    result = acl._parse_results("\n".join(["foo", "  here's a weird line "]))
+def test_parse_results_no_json(mocker, generic_acl):
+    result = generic_acl._parse_results("\n".join(["foo", "  here's a weird line "]))
 
     assert result == None
 
 
-def test_parse_results_json(mocker):
-    acl = ACL("cfg_path", "user", "password", "instance")
-    result = acl._parse_results(
+def test_parse_results_json(mocker, generic_acl):
+    result = generic_acl._parse_results(
         "\n".join(["foo", "  here's a weird line ", "{", '  "key":"value"', "}"])
     )
 
     assert {"key": "value"} == result
 
 
-def test_parse_result_warning(mocker):
-    acl = ACL("cfg_path", "user", "password", "instance")
+def test_parse_result_warning(mocker, generic_acl):
 
     with pytest.raises(ACLWarning) as err:
-        acl._parse_results(
+        generic_acl._parse_results(
             "\n".join(
                 [
                     "WARNING: this is a warning",
@@ -116,9 +111,7 @@ def test_parse_result_warning(mocker):
 
     assert "WARNING: this is a warning" in str(err.value)
 
-def test_single_token():
-    acl = ACL("cfg_path", "user", "password", "instance")
-    assert acl.single_token("thisshouldnotfail") == True
-    assert acl.single_token("this should fail") == False
-    assert acl.single_token("this^should(fail") == False
-
+def test_single_token(generic_acl):
+    assert generic_acl.single_token("thisshouldnotfail") == True
+    assert generic_acl.single_token("this should fail") == False
+    assert generic_acl.single_token("this^should(fail") == False
